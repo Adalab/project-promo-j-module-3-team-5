c6a4c242f21d945cfa9c6ea800a7ff906cb1f39e
Junyi Ji <junyiji@gmail.com>
2020-07-27 13:49:09 +0200

    Run docs

---
+++ docs/precache-manifest.6d90d4576a5aa0baac4a35c1efa87854.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b46c60b38cccf85c8ff913da40d358d7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e19c48e9d00488b5e047",
+    "url": "./static/css/main.e0343f92.chunk.css"
+  },
+  {
+    "revision": "0500e7505fc9517b1928",
+    "url": "./static/js/2.55190993.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.55190993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e19c48e9d00488b5e047",
+    "url": "./static/js/main.aec42603.chunk.js"
+  },
+  {
+    "revision": "67811d77049c1b7325ec",
+    "url": "./static/js/runtime-main.7c4ac9ee.js"
+  },
+  {
+    "revision": "5d27aa7d9abd4a4cabd637557b399695",
+    "url": "./static/media/bernard.5d27aa7d.jpg"
+  },
+  {
+    "revision": "1fcbf3588a1733e5cdf463d73c1c0b9e",
+    "url": "./static/media/green.1fcbf358.jpg"
+  },
+  {
+    "revision": "9b66b16b8d742baf8e4bf5c02364f4f7",
+    "url": "./static/media/hoagie.9b66b16b.jpg"
+  },
+  {
+    "revision": "6edf7ef5b2fa154d81542bc0bb6499c7",
+    "url": "./static/media/imagen-prueba.6edf7ef5.jpg"
+  },
+  {
+    "revision": "a9ad2df8f3dc751e1fa96bd980c981a8",
+    "url": "./static/media/laverne.a9ad2df8.jpg"
+  },
+  {
+    "revision": "d727ce18af7bea8921a0055b6fe7a5a5",
+    "url": "./static/media/logo-completom.d727ce18.png"
+  }
+]);
